ce7a1b68aa41ba6acc6b2d58af256e341f6df115
andreafontaneto <andrea.fontaneto@gmail.com>
2022-01-17 13:36:21 +0100

    aggiungo proprietà

--- index.php
+++ index.php
@@ -4,24 +4,28 @@ class Movie{
 
   public $name;
   public $genre;
+  public $year;
   public $director;
   public $movie_time;
 
 }
 
+$movie1 = new Movie();
+$movie1->name = "Ritorno al Futuro";
+$movie1->genre = "Sci-fi / Commedia";
+$movie1->year = 1985;
+$movie1->director = "Robert Zemeckis";
+$movie1->movie_time = 90;
+
+$movie2 = new Movie();
+$movie2->name = "Harry Potter";
+$movie2->genre = "Fantasy";
+$movie2->year = 2011;
+$movie2->director = "Chris Columbus";
+$movie2->movie_time = 189;
+
+var_dump($movie1);
+var_dump($movie2);
+
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP OOP EX</title>
-</head>
-<body>
-
-  <!-- Content -->
-  
-</body>
-</html>
