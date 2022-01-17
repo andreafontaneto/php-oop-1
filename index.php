4e48ac56aac7583692b8374e4082bb5646e99c1f
andreafontaneto <andrea.fontaneto@gmail.com>
2022-01-17 14:07:14 +0100

    setto campi nel documento html

--- index.php
+++ index.php
@@ -21,3 +21,24 @@ $movie2->movie_time = 189;
 
 var_dump($movie1);
 var_dump($movie2);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP OOP</title>
+</head>
+<body>
+
+  <h1>Titolo Film</h1>
+  <h3>Genere Film</h3>
+  <h4>Anno Film</h4>
+  <h4>Regista Film</h4>
+  <h4>Durata Film</h4>
+  
+</body>
+</html>
